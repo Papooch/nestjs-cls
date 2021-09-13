d130189b7734c6be6db2fbf8732811ec9134aeaf
Papooch <papoochcz@gmail.com>
2021-09-13 19:55:29 +0200

    fix: HttpAdapterHost working in tests

--- src/cls.module.ts
+++ src/cls.module.ts
@@ -1,10 +1,12 @@
 import {
     DynamicModule,
+    Logger,
     MiddlewareConsumer,
     Module,
     NestModule,
+    OnModuleInit,
 } from '@nestjs/common';
-import { HttpAdapterHost } from '@nestjs/core';
+import { HttpAdapterHost, ModuleRef } from '@nestjs/core';
 import { ClsMiddleware } from './cls.middleware';
 import { ClsService } from './cls.service';
 
@@ -15,33 +17,44 @@ class ClsModuleOptions {
 
 @Module({})
 export class ClsModule implements NestModule {
+    constructor(private readonly adapterHost: HttpAdapterHost) {}
+
     private static middlewareWildcard = '*';
     private static mountMiddleware = true;
+    private static logger = new Logger(ClsModule.name);
 
     configure(consumer: MiddlewareConsumer) {
-        if (ClsModule.mountMiddleware)
+        const adapter = this.adapterHost.httpAdapter;
+        switch (adapter.constructor.name) {
+            case 'FastifyAdapter':
+                ClsModule.logger.log(
+                    'Setting up ClsMiddleware for FastifyAdapter',
+                );
+                ClsModule.middlewareWildcard = '(.*)';
+                break;
+            default:
+                ClsModule.logger.log(
+                    'Setting up ClsMiddleware for ExpressAdapter',
+                );
+                ClsModule.middlewareWildcard = '*';
+        }
+
+        if (ClsModule.mountMiddleware) {
+            ClsModule.logger.debug(
+                'Mounting ClsMiddleware to ' + ClsModule.middlewareWildcard,
+            );
             consumer
                 .apply(ClsMiddleware)
                 .forRoutes(ClsModule.middlewareWildcard);
+        }
     }
 
     static register(options: ClsModuleOptions): DynamicModule {
         options = { ...new ClsModuleOptions(), ...options };
         this.mountMiddleware = options.mountMiddleware;
-        this.middlewareWildcard = options.http == 'fastify' ? '(.*)' : '*';
         return {
             module: ClsModule,
-            providers: [
-                {
-                    provide: ClsService,
-                    inject: [HttpAdapterHost],
-                    useFactory: (adapterHost: HttpAdapterHost) => {
-                        const adapter = adapterHost.httpAdapter;
-                        console.log('adapter name ', adapter.getInstance());
-                        return ClsService;
-                    },
-                },
-            ],
+            providers: [ClsService],
             exports: [ClsService],
         };
     }
