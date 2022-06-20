27c50fd8a41e815243e54a9ab052225d2fac006d
Ondřej Švanda <papoochcz@gmail.com>
2022-06-20 18:53:35 +0200

    deps: Udate everything to latest

--- test/gql/item/dto/recipes.args.ts
+++ test/gql/item/dto/recipes.args.ts
@@ -1,14 +1,10 @@
 import { ArgsType, Field, Int } from '@nestjs/graphql';
-import { Max, Min } from 'class-validator';
 
 @ArgsType()
 export class RecipesArgs {
     @Field(() => Int)
-    @Min(0)
     skip = 0;
 
     @Field(() => Int)
-    @Min(1)
-    @Max(50)
     take = 25;
 }
