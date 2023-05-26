bfd59d3171b62949e4f10d33b5858e77825f6ab8
Ondřej Švanda <papoochcz@gmail.com>
2023-05-26 21:14:52 +0200

    docs: fix edit page link

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -47,7 +47,7 @@ const config = {
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
                     editUrl:
-                        'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                        'https://github.com/Papooch/nestjs-cls/tree/main/docs/',
                 },
                 theme: {
                     customCss: require.resolve('./src/css/custom.css'),
