a4e2642d8a05287e040b7b3cb1e4560ed77e4b8d
Ondřej Švanda <papoochcz@gmail.com>
2024-01-29 16:31:26 +0100

    fix: rework how plugins are registered (internals)
    Previously all plugins' providers were mixed into one module,
    now each plugin gets its own module.

--- packages/core/src/lib/plugin/cls-plugin-manager.ts
+++ packages/core/src/lib/plugin/cls-plugin-manager.ts
@@ -1,41 +1,23 @@
 import { globalClsService } from '../cls-service.globals';
 import { ClsPlugin } from './cls-plugin.interface';
+import { createClsPluginModule } from './cls-plugin.module';
 
 export class ClsPluginManager {
     private static clsService = globalClsService;
     private static plugins: ClsPlugin[] = [];
 
-    static add(plugins: ClsPlugin[] = []) {
+    static registerPlugins(plugins: ClsPlugin[] = []) {
         this.plugins.push(...plugins);
+        return plugins.map((plugin) => createClsPluginModule(plugin));
     }
 
     static getPlugins() {
         return this.plugins;
     }
 
-    static getPluginImports() {
-        return this.plugins.flatMap((plugin) => plugin.imports ?? []);
-    }
-
-    static getPluginProviders() {
-        return this.plugins.flatMap((plugin) => plugin.providers ?? []);
-    }
-
     static async onClsInit() {
         for (const plugin of this.plugins) {
             await plugin.onClsInit?.(this.clsService);
-        }
-    }
-
-    static async onModuleInit() {
-        for (const plugin of this.plugins) {
-            await plugin.onModuleInit?.();
-        }
-    }
-
-    static async onModuleDestroy() {
-        for (const plugin of this.plugins) {
-            await plugin.onModuleDestroy?.();
         }
     }
 }
