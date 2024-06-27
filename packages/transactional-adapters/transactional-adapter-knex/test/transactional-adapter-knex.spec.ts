86948f5ce2b6db7be2ea2b273ee5829f2bdf321f
Ondřej Švanda <46406259+Papooch@users.noreply.github.com>
2024-06-27 21:42:12 +0200

    chore(transactional): add more tests for transactional adapters

--- packages/transactional-adapters/transactional-adapter-knex/test/transactional-adapter-knex.spec.ts
+++ packages/transactional-adapters/transactional-adapter-knex/test/transactional-adapter-knex.spec.ts
@@ -8,7 +8,7 @@ import {
 import { Inject, Injectable, Module } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import Knex from 'knex';
-import { ClsModule } from 'nestjs-cls';
+import { ClsModule, UseCls } from 'nestjs-cls';
 import { TransactionalAdapterKnex } from '../src';
 
 const KNEX = 'KNEX';
@@ -40,6 +40,13 @@ class UserService {
         @Inject(KNEX)
         private readonly knex: Knex.Knex,
     ) {}
+
+    @UseCls()
+    async withoutTransaction() {
+        const r1 = await this.userRepository.createUser('Jim');
+        const r2 = await this.userRepository.getUserById(r1.id);
+        return { r1, r2 };
+    }
 
     @Transactional()
     async transactionWithDecorator() {
@@ -147,6 +154,13 @@ describe('Transactional', () => {
     });
 
     describe('TransactionalAdapterKnex', () => {
+        it('should work without an active transaction', async () => {
+            const { r1, r2 } = await callingService.withoutTransaction();
+            expect(r1).toEqual(r2);
+            const users = await knex('user');
+            expect(users).toEqual(expect.arrayContaining([r1]));
+        });
+
         it('should run a transaction with the default options with a decorator', async () => {
             const { r1, r2 } = await callingService.transactionWithDecorator();
             expect(r1).toEqual(r2);
--- packages/transactional-adapters/transactional-adapter-kysely/test/transactional-adapter-kysely.spec.ts
+++ packages/transactional-adapters/transactional-adapter-kysely/test/transactional-adapter-kysely.spec.ts
@@ -9,7 +9,7 @@ import { Inject, Injectable, Module } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { execSync } from 'child_process';
 import { Generated, Kysely, PostgresDialect } from 'kysely';
-import { ClsModule } from 'nestjs-cls';
+import { ClsModule, UseCls } from 'nestjs-cls';
 import { Pool } from 'pg';
 import { TransactionalAdapterKysely } from '../src';
 
@@ -62,6 +62,13 @@ class UserService {
         @Inject(KYSELY)
         private readonly kysely: Kysely<Database>,
     ) {}
+
+    @UseCls()
+    async withoutTransaction() {
+        const r1 = await this.userRepository.createUser('Jim');
+        const r2 = await this.userRepository.getUserById(r1.id);
+        return { r1, r2 };
+    }
 
     @Transactional()
     async transactionWithDecorator() {
@@ -186,6 +193,16 @@ describe('Transactional', () => {
     }, 60_000);
 
     describe('TransactionalAdapterKysely', () => {
+        it('should work without an active transaction', async () => {
+            const { r1, r2 } = await callingService.withoutTransaction();
+            expect(r1).toEqual(r2);
+            const users = await kyselyDb
+                .selectFrom('user')
+                .selectAll()
+                .execute();
+            expect(users).toEqual(expect.arrayContaining([r1]));
+        });
+
         it('should run a transaction with the default options with a decorator', async () => {
             const { r1, r2 } = await callingService.transactionWithDecorator();
             expect(r1).toEqual(r2);
