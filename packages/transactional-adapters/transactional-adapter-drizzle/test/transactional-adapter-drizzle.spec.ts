248cf2de7a00db2e52f509070783c42e182c024c
Frederic Portaria-Janicki <frederic@fjanicki.ca>
2024-10-10 17:43:19 -0400

    feat(drizzle): add basic support for drizzle-orm postgres

---
+++ packages/transactional-adapters/transactional-adapter-drizzle/test/transactional-adapter-drizzle.spec.ts
@@ -0,0 +1,7 @@
+const DRIZZLE_TOKEN = 'DRIZZLE';
+
+describe('TransactionalAdapter Drizzle', () => {
+    it('Should be able to create a transactional adapter', async () => {
+        return true;
+    });
+});
