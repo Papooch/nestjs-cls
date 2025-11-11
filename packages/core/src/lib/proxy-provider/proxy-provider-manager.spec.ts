2e67c25a3ac44f983e0ed396d9d4bc04bc54eca9
Matthew Bull <matt.bull@beautypie.com>
2025-11-11 22:40:21 +0000

    chore: allow prettier to do its thing

--- packages/core/src/lib/proxy-provider/proxy-provider-manager.spec.ts
+++ packages/core/src/lib/proxy-provider/proxy-provider-manager.spec.ts
@@ -1,90 +1,91 @@
-import { globalClsService } from "../cls-service.globals";
-import { ProxyProviderManager } from "./proxy-provider-manager";
+import { globalClsService } from '../cls-service.globals';
+import { ProxyProviderManager } from './proxy-provider-manager';
 
 describe('ProxyProviderManager', () => {
-    afterEach(() => ProxyProviderManager.reset())
+    afterEach(() => ProxyProviderManager.reset());
     describe('createProxyProvider', () => {
         it('is defined', () => {
             expect(ProxyProviderManager.createProxyProvider).toBeDefined();
         });
 
         it('returns a provider', () => {
-            const providerToken = Symbol("example-provider")
+            const providerToken = Symbol('example-provider');
 
-            expect(ProxyProviderManager.createProxyProvider({
-                provide: providerToken,
-                useFactory: () => ({}),
-            })).toEqual(expect.objectContaining({
-                provide: providerToken,
-                useFactory: expect.any(Function),
-            }));
+            expect(
+                ProxyProviderManager.createProxyProvider({
+                    provide: providerToken,
+                    useFactory: () => ({}),
+                }),
+            ).toEqual(
+                expect.objectContaining({
+                    provide: providerToken,
+                    useFactory: expect.any(Function),
+                }),
+            );
         });
 
-        describe("the provider factory", () => {
-            it("allows access to the underlying provider properties", async () => {
-                await globalClsService.run(
-                    async () => {
-                        const provider = {
-                            key: "value",
-                        }
-                        const providerToken = Symbol("example-provider")
-                        const { useFactory } = ProxyProviderManager.createProxyProvider({
+        describe('the provider factory', () => {
+            it('allows access to the underlying provider properties', async () => {
+                await globalClsService.run(async () => {
+                    const provider = {
+                        key: 'value',
+                    };
+                    const providerToken = Symbol('example-provider');
+                    const { useFactory } =
+                        ProxyProviderManager.createProxyProvider({
                             provide: providerToken,
-                            useFactory: () => provider
+                            useFactory: () => provider,
                         });
 
-                        const instance = useFactory()
+                    const instance = useFactory();
 
-                        ProxyProviderManager.init()
-                        await ProxyProviderManager.resolveProxyProviders()
+                    ProxyProviderManager.init();
+                    await ProxyProviderManager.resolveProxyProviders();
 
-                        expect(instance.key).toBe(provider.key)
-                    }
-                )
+                    expect(instance.key).toBe(provider.key);
+                });
             });
-            it("binds function properties", async () => {
-                await globalClsService.run(
-                    async () => {
-                        const provider = {
-                            fn() {
-                                return this
-                            }
-                        }
-                        const providerToken = Symbol("example-provider")
-                        const { useFactory } = ProxyProviderManager.createProxyProvider({
+            it('binds function properties', async () => {
+                await globalClsService.run(async () => {
+                    const provider = {
+                        fn() {
+                            return this;
+                        },
+                    };
+                    const providerToken = Symbol('example-provider');
+                    const { useFactory } =
+                        ProxyProviderManager.createProxyProvider({
                             provide: providerToken,
-                            useFactory: () => provider
+                            useFactory: () => provider,
                         });
 
-                        const instance = useFactory()
+                    const instance = useFactory();
 
-                        ProxyProviderManager.init()
-                        await ProxyProviderManager.resolveProxyProviders()
+                    ProxyProviderManager.init();
+                    await ProxyProviderManager.resolveProxyProviders();
 
-                        expect(instance.fn()).toBe(provider)
-                    }
-                )
+                    expect(instance.fn()).toBe(provider);
+                });
             });
-            it("preserves properties on function properties", async () => {
-                await globalClsService.run(
-                    async () => {
-                        const provider = {
-                            fn: Object.assign(() => "hello", { info: () => null })
-                        }
-                        const providerToken = Symbol("example-provider")
-                        const { useFactory } = ProxyProviderManager.createProxyProvider({
+            it('preserves properties on function properties', async () => {
+                await globalClsService.run(async () => {
+                    const provider = {
+                        fn: Object.assign(() => 'hello', { info: () => null }),
+                    };
+                    const providerToken = Symbol('example-provider');
+                    const { useFactory } =
+                        ProxyProviderManager.createProxyProvider({
                             provide: providerToken,
-                            useFactory: () => provider
+                            useFactory: () => provider,
                         });
 
-                        const instance = useFactory()
+                    const instance = useFactory();
 
-                        ProxyProviderManager.init()
-                        await ProxyProviderManager.resolveProxyProviders()
+                    ProxyProviderManager.init();
+                    await ProxyProviderManager.resolveProxyProviders();
 
-                        expect(instance.fn.info).toEqual(expect.any(Function))
-                    }
-                )
+                    expect(instance.fn.info).toEqual(expect.any(Function));
+                });
             });
         });
     });
